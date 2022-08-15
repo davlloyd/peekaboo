fb471a60c4e57c7c9852ca3a37696762113f1964
davlloyd <davidlloyd0@gmail.com>
2022-08-15 18:20:47 +1000

    loggin additions

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
+import sys
 from flask import current_app
 from peekaboo import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+print('DB URI: {0}'.format(app.config["SQLALCHEMY_DATABASE_URI"]), file=sys.stdout)
 
 if __name__ == "__main__":
 	app.run(debug=app.config['DEBUG'],host='0.0.0.0', port=int(os.getenv('PORT', '80')))
