5bf52ae1a41951d58b3ff3ec73b88b2786696e6f
davlloyd <davidlloyd0@gmail.com>
2022-08-19 12:29:42 +1000

    Node update

--- peekaboo/__init__.py
+++ peekaboo/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from peekaboo import config
 import sys
-from flask_request_id_header.middleware import RequestID
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -11,7 +10,6 @@ app = Flask(__name__)
 def create_app(config_name):
     app.config.from_object(config.config[config_name])
     config.config[config_name].init_app(app)
-    RequestID(app)
     db.init_app(app)
 
     print('Import blueprints', file=sys.stdout)
